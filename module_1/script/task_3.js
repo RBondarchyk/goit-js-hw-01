fc779a9ab376b0f828a477a6efa004f1141bc22f
RBondarchyk <bondarchykra@gmail.com>
2020-01-30 15:15:02 +0200

    tree

--- module_1/script/task_3.js
+++ module_1/script/task_3.js
@@ -4,13 +4,14 @@ const adminInput = prompt('Введите пароль');
 console.log(adminInput);
 if (adminInput === null) {
   alert('Отменено пользователем!');
-}
-const normalizeAdminInput = adminInput.toLowerCase();
-console.log(normalizeAdminInput);
-message = ADMIN_PASSWORD === normalizeAdminInput;
-console.log(message);
-if (message === true) {
-  alert('Добро пожаловать!');
 } else {
-  alert('Доступ запрещен, неверный пароль!');
+  const normalizeAdminInput = adminInput.toLowerCase();
+  console.log(normalizeAdminInput);
+  message = ADMIN_PASSWORD === normalizeAdminInput;
+  console.log(message);
+  if (message === true) {
+    alert('Добро пожаловать!');
+  } else {
+    alert('Доступ запрещен, неверный пароль!');
+  }
 }
